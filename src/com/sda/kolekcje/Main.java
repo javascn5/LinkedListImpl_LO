ba31af47aa2e3f47622008522981e11ef585a33a
lukaszog <lukasz.ogan@gmail.com>
2018-10-21 13:37:53 +0200

    prepare linkedlist implementation

--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -1,8 +1,24 @@
 package com.sda.kolekcje;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //ArrayList - implementacja tablicowa
+        //LinkedList - implementa wiazana
+
+        List<String> list = new LinkedList<>();
+
+        list.add("Jan");
+        list.add("Marcin");
+        list.add("Karol");
+
+        for (String l: list){
+            System.out.println(l);
+        }
+
     }
 }
